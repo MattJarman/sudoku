8217a7dbc8ac1b9c488ce9cdc208b631c94e049b
Matt <mattjarman22@outlook.com>
2022-01-05 13:44:27 +0000

    Create eslint config

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,11 +1,34 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard',
+    'next',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      tsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
   settings: {
     next: {
-      rootDir: ["apps/*/", "packages/*/"],
-    },
+      rootDir: ['apps/*/', 'packages/*/']
+    }
   },
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-};
+    '@next/next/no-html-link-for-pages': 'off'
+  }
+}
