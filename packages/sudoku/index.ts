bf55803604ad45d578da0d1bc52e70a4a7a9007f
Matt <mattjarman22@outlook.com>
2022-01-07 17:30:15 +0000

    Update sudoku solver

--- packages/sudoku/index.ts
+++ packages/sudoku/index.ts
@@ -1,64 +1,74 @@
-const SUDOKU_LENGTH = 9
+const SUDOKU_BOARD_LENGTH = 9
 const SUDOKU_GRID_LENGTH = 3
 
-export const solve = (grid: number[][], row = 0, column = 0): Array<boolean | number[][]> => {
-  if (row === SUDOKU_LENGTH - 1 && column === SUDOKU_LENGTH) {
-    return [true, grid]
+export const solve = (board: number[][]): Array<boolean | number[][]> => {
+  const empty = findEmpty(board)
+  if (!empty) {
+    return [true, board]
   }
 
-  if (column === SUDOKU_LENGTH) {
-    row++
-    column = 0
-  }
+  const [row, column] = empty
+  for (let i = 1; i <= board.length; i++) {
+    if (isValid(board, row, column, i)) {
+      board[row][column] = i
 
-  if (grid[row][column] !== 0) {
-    return solve(grid, row, column + 1)
-  }
-
-  for (let i = 1; i <= SUDOKU_LENGTH; i++) {
-    if (isValid(grid, row, column, i)) {
-      grid[row][column] = i
-
-      const [solved] = solve(grid, row, column + 1)
+      const [solved] = solve(board)
       if (solved) {
-        return [true, grid]
+        return [true, board]
       }
-    }
 
-    grid[row][column] = 0
+      board[row][column] = 0
+    }
   }
 
-  return [false, grid]
+  return [false, board]
 }
 
-export const createGrid = (sudokuString: string): number[][] => {
-  const grid = Array.from(new Array(SUDOKU_LENGTH), () => [])
+export const createBoard = (sudokuString: string): number[][] => {
+  const board = Array.from(new Array(SUDOKU_BOARD_LENGTH), () => [])
   const rows = sudokuString.split('')
 
   for (const [index, value] of rows.entries()) {
-    grid[Math.floor(index / 9)].push(parseInt(value))
+    board[Math.floor(index / 9)].push(parseInt(value))
   }
 
-  return grid
+  return board
 }
 
-export const isValid = (grid: number[][], row: number, column: number, number: number): boolean => {
-  for (let i = 0; i < SUDOKU_LENGTH; i++) {
-    if (grid[row][i] === number || grid[i][column] === number) {
+export const isValid = (
+  board: number[][],
+  row: number,
+  column: number,
+  number: number
+): boolean => {
+  for (let i = 0; i < SUDOKU_BOARD_LENGTH; i++) {
+    if (board[row][i] === number || board[i][column] === number) {
       return false
     }
   }
 
-  const startRow = row - row % SUDOKU_GRID_LENGTH
-  const startColumn = column - column % SUDOKU_GRID_LENGTH
+  const startRow = row - (row % SUDOKU_GRID_LENGTH)
+  const startColumn = column - (column % SUDOKU_GRID_LENGTH)
 
   for (let i = 0; i < SUDOKU_GRID_LENGTH; i++) {
     for (let j = 0; j < SUDOKU_GRID_LENGTH; j++) {
-      if (grid[i + startRow][j + startColumn] === number) {
+      if (board[i + startRow][j + startColumn] === number) {
         return false
       }
     }
   }
 
   return true
+}
+
+const findEmpty = (board: number[][]): number[] | boolean => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === 0) {
+        return [i, j]
+      }
+    }
+  }
+
+  return false
 }
