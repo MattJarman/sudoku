a4b97b5c3e55c706446de031bf3db2d1c98380cd
Matt <mattjarman22@outlook.com>
2022-01-10 13:00:27 +0000

    Add tests for sudoku

---
+++ packages/sudoku/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    preset: 'ts-jest',
+    clearMocks: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'babel',
+    testEnvironment: 'node',
+    globals: {
+        'ts-jest': {
+            isolatedModules: true
+        }
+    },
+    testMatch: ['**/*.test.ts'],
+    resetModules: true,
+    coveragePathIgnorePatterns: ['node_modules']
+}
