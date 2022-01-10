122f30826bcd379cd78e5f01476971680f89aa33
Matt <mattjarman22@outlook.com>
2022-01-10 13:00:01 +0000

    Add jest to eslint

--- packages/config/eslint-app.js
+++ packages/config/eslint-app.js
@@ -7,6 +7,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
     'standard'
   ],
   parser: '@typescript-eslint/parser',
